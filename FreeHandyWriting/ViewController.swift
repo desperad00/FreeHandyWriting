6ccf71c53cfe88edce677210198d93363867b4a8
韩杰 <510541175@qq.com>
2018-05-08 16:44:32 +0800

    Initial Commit

---
+++ FreeHandyWriting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreeHandyWriting
+//
+//  Created by han on 2018/5/8.
+//  Copyright © 2018年 puppytech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
